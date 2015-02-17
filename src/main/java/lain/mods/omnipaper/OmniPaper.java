fa2b2bdf18f6b73c9748569aa865745dc59d1b1f
Lain MultipleInstances <zlainsama@163.com>
2015-02-17 19:38:42 +0800

    data pattern

--- src/main/java/lain/mods/omnipaper/OmniPaper.java
+++ src/main/java/lain/mods/omnipaper/OmniPaper.java
@@ -1,11 +1,15 @@
 package lain.mods.omnipaper;
 
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import com.google.common.collect.Lists;
 
 @Mod(modid = "OmniPaper", useMetadata = true)
 public class OmniPaper
@@ -36,6 +40,32 @@ public class OmniPaper
         }
 
     }
+
+    private static List<String> getData(ItemStack stack)
+    {
+        return getData(stack.getDisplayName());
+    }
+
+    private static List<String> getData(String string)
+    {
+        Matcher matcher = dataPattern.matcher(unhideString(string));
+        List<String> result = Lists.newLinkedList();
+        while (matcher.find())
+        {
+            String data = matcher.group();
+            data.substring(6, data.length() - 1);
+            if (!data.isEmpty())
+                result.add(data);
+        }
+        return result;
+    }
+
+    private static String unhideString(String string)
+    {
+        return string.replace("\u00a7", "");
+    }
+
+    private static Pattern dataPattern = Pattern.compile("(\\[DATA=.*\\])");
 
     @Mod.EventHandler
     public void init(FMLPreInitializationEvent event)
